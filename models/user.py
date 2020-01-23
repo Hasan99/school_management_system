410f314ef1eff886faf01647d5def8e72b1013fa
Hasan99 <hasan_libra95@hotmail.com>
2020-01-24 01:28:54 +0500

    changes in user.py

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from main import db, ma
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey("role.role_id"))
 
 
